37ae2e9b70fc04c468fa99d700a4a09d918104fb
Ivan <ikolpakoff@gmail.com>
2016-07-09 21:44:57 +0300

    add fxml and choose scene in start method

--- src/main/java/ru/ikolpakoff/Main.java
+++ src/main/java/ru/ikolpakoff/Main.java
@@ -1,6 +1,9 @@
 package ru.ikolpakoff;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application{
@@ -11,7 +14,13 @@ public class Main extends Application{
 
     public void start(Stage primaryStage) throws Exception {
 
+        Parent root = FXMLLoader.load(getClass().getResource("/fxmls/mainWindow.fxml"));
+
         primaryStage.setTitle("simpleWindow");
+        primaryStage.setWidth(400);
+        primaryStage.setHeight(300);
+        primaryStage.setResizable(false);
+        primaryStage.setScene(new Scene(root));
         primaryStage.show();
 
     }
