86359fe4a267b3ebb206ea0e8b67fac9da6261a0
Ivan <ikolpakoff@gmail.com>
2016-07-11 21:06:40 +0300

    add setupText

--- src/main/java/ru/ikolpakoff/controllers/MainController.java
+++ src/main/java/ru/ikolpakoff/controllers/MainController.java
@@ -1,4 +1,18 @@
 package ru.ikolpakoff.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class MainController {
+
+    @FXML private Label name;
+    @FXML private TextField field;
+    @FXML private Button button;
+
+    public void setupText(ActionEvent actionEvent) {
+        name.setText(field.getText());
+    }
 }
